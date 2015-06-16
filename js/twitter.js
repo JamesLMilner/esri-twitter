c830fcd05affb0dfd6564bfd2649ac46906bbdda
James Milner <jmilner@esriuk.com>
2015-06-16 19:19:22 +0100

    Updated to pure dojo, removed jquery

--- js/twitter.js
+++ js/twitter.js
@@ -1,5 +1,10 @@
 
     require([
+        "dojo/dom",
+        "dojo/query",
+        "dojo/dom-form",
+        "dojo/on",
+        "dojo/request",
         "esri/map",
         "esri/geometry/Point",
         "esri/graphic",
@@ -8,9 +13,11 @@
         "esri/InfoTemplate",
         "esri/geometry/webMercatorUtils",
         "esri/dijit/Search",
-        "dojo/domReady!"],
+        "dojo/domReady!"
+        ],
     function(
-        Map, Point, Graphic, GraphicsLayer, PictureMarkerSymbol, InfoTemplate, webMercatorUtils, Search
+        dom, query, domForm, on, request, Map, Point, Graphic, GraphicsLayer,
+        PictureMarkerSymbol, InfoTemplate, webMercatorUtils, Search
     ) {
         var map;
         var tweetLayer;
@@ -18,53 +25,48 @@
         if (navigator.geolocation) {
             console.log("Geolocation is supported")
             navigator.geolocation.getCurrentPosition( function(position) {
-
                 var lat = position.coords.latitude;
                 var lon = position.coords.longitude;
-                map = Map("map",{
-                            basemap:"gray",
-                            center:[lon, lat],
-                            zoom: 8
-                        });
-
-                map.on("load", function() {
-                            var s = new Search( { map: map }, "search");
-                            s.startup();
-                        });
+                map = generateMap(lon, lat);
             });
 
-        } else {
+        }
+        else {
             console.log("Geolocation not supported")
-            map = Map("map",{
+            map = generateMap(0, 30);
+        }
+
+        function generateMap(lon, lat) {
+            map =  Map("map",{
                 basemap:"gray",
-                center:[0, 20],
+                center:[lon, lat ],
                 zoom: 8
-            });
+            })
 
             map.on("load", function() {
                 var s = new Search( { map: map }, "search");
                 s.startup();
             });
+
+            return map;
         }
 
         function getTweets(tag, count, center,radius) {
 
-            $.ajax({
-                url: "gettweets.php",
+            request.post("gettweets.php", {
                 data: {
-                    "tag": tag,
-                    "count": count,
-                    "center": center,
-                    "radius" : radius
+                    tag: tag,
+                    count: count,
+                    center: center,
+                    radius : radius
                 },
-                success: function(tweets) {
+                handleAs : "json"
+            }).then( function(tweets){
+                console.log(tweets)
                     map.graphicsLayerIds[0]
-                    console.log(map);
-                    console.log(tweets);
 
                     if (tweetLayer && tweetLayer != "undefined") { tweetLayer.clear() }
                     tweetLayer = new GraphicsLayer();
-                    //console.log(tweets);
                     var symbol = new PictureMarkerSymbol("imgs/twitter_icon.png", 16, 13);
 
                     tweets.statuses.forEach( function(tweet, index) {
@@ -72,55 +74,48 @@
                         if (geo && geo.coordinates.length == 2 ) {
                             var longitude = geo.coordinates[1];
                             var latitude =  geo.coordinates[0];
-                            //console.log(longitude, latitude)
+
                             var point = new Point(longitude, latitude);
                             var date;
                             if (tweet.created_at) {
                                 date =  tweet.created_at.replace("+0000", "");
 
                             }
-
-
                             var infoTemplate = new InfoTemplate(
                                 "<a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.user.name + "</a>",
                                 "<div class='userphoto'><img class='photo' src='" + tweet.user.profile_image_url + "'></div><div class='usertext'>" + tweet.text  + "<br><br><br><b>Tweeted: </b> " + date + "</div>"
                             );
 
-                            //console.log(infoTemplate);
                             var graphic = new Graphic(point, symbol, "" , infoTemplate);
-                            //console.log(point, graphic);
                             tweetLayer.add(graphic);
                         }
                     });
 
                     map.addLayer(tweetLayer);
                     console.log("Done adding tweets");
-                },
-                dataType: "json"
+
             });
-      }
+        }
 
+        // Attach the onsubmit event handler of the form
+        on(dom.byId('form'), "submit", function(evt){
 
-      $('#form').on('submit', function(e) {
-          e.preventDefault();
-          console.log("Map", map)
-          var data = $("#form :input").serializeArray();
-          var tag = data[0].value;
-          var count = 100;  //+data[1].value;
-          var objectCenter = webMercatorUtils.webMercatorToGeographic(map.extent.getCenter());
-          var center = objectCenter.x + "," + objectCenter.y;
-          var radius = (map.extent.getWidth() / 1000) / 2;
-          // The width is in meters so divide by 1000, and to get a good radius divide by 2
+            // prevent the page from navigating after submit
+            evt.stopPropagation();
+            evt.preventDefault();
 
-          console.log(tag, count, center);
+            var tag = dom.byId('tag').value;
+            var count = 100;  //+data[1].value;
+            var objectCenter = webMercatorUtils.webMercatorToGeographic(map.extent.getCenter());
+            var center = objectCenter.x + "," + objectCenter.y;
+            var radius = (map.extent.getWidth() / 1000) / 2;
+            // The width is in meters so divide by 1000, and to get a good radius divide by 2
 
-          if (typeof tag == "string" && isNaN(count) == false ) {
-              console.log("Getting tweets");
-              $("#tag").css("background-color", "white");
-              $("#count").css("background-color", "white");
-              getTweets(tag, count, center, radius);
-          }
+            query("#tag").style("background-color", "white");
+            query("#count").style("background-color", "white");
+            console.log("Getting tweets", tag, count, center, radius);
+            getTweets(tag, count, center, radius);
 
-      });
+        });
 
     });
